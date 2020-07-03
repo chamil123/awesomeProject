167f420a4ed6a35a69646dc74d33fce1cf813000
chamil <chamiljay88@gmail.com>
2020-07-03 17:40:01 +0530

    5.35 editing

--- android/app/src/screens/LoadingScreen.js
+++ android/app/src/screens/LoadingScreen.js
@@ -1,10 +1,20 @@
 import React, {Component} from 'react';
 import {View,Text,StyleSheet,Animated} from 'react-native';
+import { TextInput } from 'react-native-gesture-handler';
  class LoadingScreen extends Component{
     render(){
         return(
            <View style={style.container}>
-               <Text style={style.logoText}>Loadings Screen</Text>
+               <View style={style.textfields}>
+                   <TextInput style={style.input}>
+                       placeholder="username"
+                       returnKeyType="next"
+                       onSubmitEditing={()=>this.passwordInput.focus()}
+
+                   </TextInput>
+
+               </View>
+              
            </View>
         )
     }
@@ -24,5 +34,16 @@ const style=StyleSheet.create({
         fontSize:25,
         marginTop:1,
         fontWeight:'300',
+    },
+    input:{
+        paddingLeft:20,
+        borderRadius:50,
+        height:50,
+        fontSize:25,
+        backgroundColor:'white',
+        borderColor:'#1abc9c',
+        borderWidth:1,
+        marginBottom:20,
+        color:'#34495e'
     }
 });
